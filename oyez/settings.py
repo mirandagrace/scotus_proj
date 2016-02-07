13643857ec2982b5c34ab447b46eef000c051e1f
Miranda <mirandaseitzmcleese@gmail.com>
2016-02-06 21:15:29 -0500

    Add oyez scrapy project

---
+++ oyez/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for oyez project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'oyez'
+
+SPIDER_MODULES = ['oyez.spiders']
+NEWSPIDER_MODULE = 'oyez.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'oyez (+http://www.yourdomain.com)'
