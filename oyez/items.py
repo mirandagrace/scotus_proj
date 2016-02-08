182c66579c8c77a83d24c3bb9415a669a246cf65
Miranda <mirandaseitzmcleese@gmail.com>
2016-02-08 00:11:33 -0500

    Add json parsing to the parse_case function

--- oyez/items.py
+++ oyez/items.py
@@ -6,9 +6,40 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.loader.processor import TakeFirst
+
+class CaseItem(scrapy.Item):
+  # define the fields for your item here like:
+  name = scrapy.Field()
+  granted_date = scrapy.Field()
+  dec_date = scrapy.Field()
+  facts = scrapy.Field()
+  decision = scrapy.Field()
+  holding = scrapy.Field()
+  syllabus = scrapy.Field()
+  docket = scrapy.Field()
+  citation = scrapy.Field()
+  conclusion = scrapy.Field()
+
+class AdvocateItem(scrapy.Item):
+  side = scrapy.Field()
+  name = scrapy.Field()
+
+class TurnItem(scrapy.Item):
+  speaker = scrapy.Field()
+  text = scrapy.Field()
+  context = scrapy.Field()
+
+class QuestionItem(scrapy.Item):
+  text = scrapy.Field()
+  disposition = scrapy.Field()
 
 
-class OyezItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
+
+
+  
+
+
+
--- oyez/settings.py
+++ oyez/settings.py
@@ -12,6 +12,8 @@ BOT_NAME = 'oyez'
 
 SPIDER_MODULES = ['oyez.spiders']
 NEWSPIDER_MODULE = 'oyez.spiders'
+CONCURRENT_REQUESTS = 4
+DOWNLOAD_DELAY = 1
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'oyez (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36'
