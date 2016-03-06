1dc334909b11c382b2be11366c146b122b508788
Miranda <mirandaseitzmcleese@gmail.com>
2016-03-06 13:24:30 -0500

    Increased efficiency of database writes: :

--- scotus/learn/vote_predict.py
+++ scotus/learn/vote_predict.py
@@ -1,8 +1,14 @@
 from sqlalchemy.sql import select, and_
 from pandas.io import sql
-from ..db.models import *
+from scotus.db.models import *
 import numpy as np
 import pandas
+
+def gender_encode(x):
+  if x != u"F":
+    return 1
+  else:
+    return 0
 
 def get_transcript_data(db):
     turn_query = select([Turn, Advocacy.side, Argument.date, Argument.case_id,
@@ -20,6 +26,7 @@ def get_transcript_data(db):
     turn_data.drop(['time_start', 'time_end', 'turn_number'], inplace=True, axis=1)
     turn_data['interrupted'] = turn_data['text'].str.endswith('--')
     turn_data['interruption'] = turn_data['interrupted'].shift(1).fillna(False)
+    turn_data['gender'] = turn_data['gender'].apply(gender_encode)
     return turn_data
     
 def case_agg(group):
@@ -31,7 +38,8 @@ def case_agg(group):
         'speaking_time': [group['length'].sum()],
         'interrupting': [group['interruption'].sum()],
         'interrupted': [group['interrupted'].sum()],
-        'document': [group['text'].str.cat(sep=' ')]})        
+        'document': [group['text'].str.cat(sep=' ')]}) 
+        
 def justice_agg(group):
     return pandas.DataFrame({
         'number_turns': [len(group['text'])],
@@ -47,13 +55,17 @@ def case_justice_features(data):
     features = case_side_groups.apply(justice_agg).unstack('side')
     features.columns = ['justice_'+'_'.join(col).strip() for col in features.columns.values]
     features.reset_index(inplace=True)
-    features.drop(['level_2'], inplace=True, axis=1)
+    try:
+      features.drop(['level_2'], inplace=True, axis=1)
+    except ValueError:
+      pass
     return features
     
 def case_transcript_features(data):
     data.drop(['kind', 'justice_id'], axis=1, inplace=True)
     case_side_groups = data.groupby(['case_id', 'side'])
-    features = case_side_groups.apply(case_agg).fillna(value={'document':'', 'gender':'M'}).fillna(value=0).reset_index()
+    features = case_side_groups.apply(case_agg).fillna(value={'document':'', 'gender':'M'}).fillna(value=0)
+    print features.columns.values
     features.drop(['level_2'], inplace=True, axis=1)
     features = features.pivot('case_id', 'side')
     features.columns = ['_'.join(col).strip() for col in features.columns.values]
@@ -66,11 +78,12 @@ def get_case_data(db):
                                       Case.winning_side,
                                       Case.facts,
                                       Case.dec_date,
+                                      Case.dec_type,
                                       Case.id]), con=db.engine, index_col='id',
                                       parse_dates=['dec_date'])
                                       
 def get_vote_data(db):
-    return sql.read_sql_query(select([Vote.id, Vote.vote, Vote.justice_id, Vote.case_id]).where(Vote.vote != u'none'),
+    return sql.read_sql_query(select([Vote.id, Vote.vote, Vote.justice_id, Vote.case_id]),
                               con=db.engine, index_col='id')
 
 def get_vote_side(x):
@@ -82,7 +95,7 @@ def get_vote_side(x):
         elif winning_side == 'respondent':
             return 'respondent'
     elif vote == 'minority':
-        if winning_side == 'petitioner':
+        if winning_side == 'petitioner':    
             return 'respondent'
         elif winning_side == 'respondent':
             return 'petitioner'
@@ -91,6 +104,7 @@ def get_vote_side(x):
         
 def fetch_data(db):
     transcript_data = get_transcript_data(db)
+    print transcript_data.head()
     case_data = transcript_data.copy()[transcript_data.kind != 'justice']
     case_features = case_transcript_features(case_data)
     justice_data = transcript_data.copy()[transcript_data.kind == 'justice']
@@ -103,10 +117,14 @@ def fetch_data(db):
     vote_case_data = vote_data.join(case_data, on='case_id')
     justice_vote_case_data = pandas.merge(vote_case_data, justice_features,
                              on=['justice_id', 'case_id'], how='left')
+    justice_vote_case_data.loc[(justice_vote_case_data['vote'] != 'none') & (justice_vote_case_data['dec_type'] == 'per curiam'), ('vote')] = u'majority'
+    votes = justice_vote_case_data[['vote', 'case_id']].groupby(['case_id'],  as_index=False).agg(lambda x: len(x[x['vote']=='majority']))
+    votes.rename(columns={'vote':'majority'}, inplace=True)
+    justice_vote_case_data = pandas.merge(justice_vote_case_data, votes, on=['case_id'])
     justice_vote_case_data['vote'] = justice_vote_case_data.apply(get_vote_side, axis = 1)
     justice_vote_case_data.rename(columns={'dec_date': 'date_decided'}, inplace=True)
     justice_vote_case_data.drop(['case_id', 'winning_side'], inplace=True, axis=1)
-    justice_vote_case_data.dropna(subset=['facts', 'date_argued', 'document_petitioner', 'document_respondent'], 
+    justice_vote_case_data.dropna(subset=['facts', 'date_argued', 'document_petitioner', 'document_respondent', 'majority'], 
                                   inplace=True)
     justice_vote_case_data.fillna(value={'justice_document_petitioner':'', 
                                    'justice_document_respondent':''}, inplace=True)
